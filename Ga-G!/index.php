ddde687a44980dbbf45c5c03f68bd836e41151cb
Wima Putra Mansyah <digohebat@gmail.com>
2021-09-12 19:50:20 +0700

    List Navbar
    Update list pada navbar

--- Ga-G!/index.php
+++ Ga-G!/index.php
@@ -22,10 +22,15 @@
         </div>
         <div id="mainListDiv" class="main_list">
             <ul class="navlinks">
-                <li><a href="#">About</a></li>
-                <li><a href="#">Portfolio</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Contact</a></li>
+            <li>
+                <a href="#">Explore</a>
+            </li>
+            <li>
+                <a href="#">Daftar</a>
+            </li>
+            <li>
+                <a href="#">Masuk</a>
+            </li>
             </ul>
         </div>
         <span class="navTrigger">
