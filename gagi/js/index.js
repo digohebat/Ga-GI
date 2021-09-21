26afe6e45da1cc41cfc30902a17e48f439e7dca9
Prasanti Oktaviana <prasanti.okta@gmail.com>
2021-09-21 20:22:13 +0700

    replace gagi

--- gagi/js/index.js
+++ gagi/js/index.js
@@ -1,16 +1,17 @@
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
     console.log("Clicked menu");
-    $("#mainListDiv").toggleClass("show_list");
-    $("#mainListDiv").fadeIn();
+    $("#menu").toggleClass("show_list");
+    $("#menu").fadeIn();
 
 });
 
 $(window).scroll(function() {
     if ($(document).scrollTop() > 50) {
-        $('.nav').addClass('affix');
+        $('.nav').addClass('solid');
+        $('.navTrigger').addClass('solid');
         console.log("OK");
     } else {
-        $('.nav').removeClass('affix');
+        $('.nav').removeClass('solid');
     }
 });
